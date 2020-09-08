68e2562530e7b94b6ab59638e86ee6f1486e3fa6
hansenjl <jenn.leigh.hansen@gmail.com>
2020-09-08 09:48:15 -0500

    starter code

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <div id="header">
+            <h1>Shopping</h1>
+            <p>View Cart</p>
+        </div>
+    )
+}
+
+export default Header
