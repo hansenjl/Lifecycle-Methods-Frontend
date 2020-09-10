822d23baf17511b691dcf4eaa849f69ebd526539
hansenjl <jenn.leigh.hansen@gmail.com>
2020-09-10 15:15:16 -0700

    hoooks

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,50 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-function Header(props){
-    const [count, setCount] = useState(0)
-    // const [time, setTime] = useState(60)
-    // const [timer, setTimer] = useState(null)
+function Header(props) {
+  const [name, setName] = useState("Andrew");
+  const [age, setAge] = useState(0);
+  const [time, setTime] = useState(0);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("in timer");
+      setTime(time => time + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []); //componentDidMount
 
-//    useEffect(()=>{
-//        console.log("in use effect")
-//        setTimer(setInterval(()=>{
-//             setTime( time - 1 )
-//        }, 1000))
-//    }, [timer])
+  useEffect(() => {
+    console.log("in use effect");
+    document.title = name;
+    return () => {
+      // cleanup
+    };
+  }, [name]);
 
-    return(
-        <div id="header">
+  //    useEffect(()=>{
+  //        console.log("in use effect")
+  //        setTimer(setInterval(()=>{
+  //             setTime( time - 1 )
+  //        }, 1000))
+  //    }, [timer])
 
-            <h1 onClick={()=>{setCount(count + 1)}}>{count} likes</h1>
-            <h1>Shopping Central</h1>
-            <p onClick={props.changePage} >View {props.page === "Items" ? "Cart" : "Items"}</p>
-        </div>
-    )
+  return (
+    <div id="header">
+      <h1>
+        Name: {name} Age: {age}
+      </h1>
+      <h2>{time}</h2>
+      <button onClick={() => setAge(age + 1)}>Happy Birthday</button>
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        value={name}
+      />
+    </div>
+  );
 }
 
-export default Header
+export default Header;
